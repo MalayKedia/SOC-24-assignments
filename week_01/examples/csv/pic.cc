3d3a8842344c909034b8acda5f48cbb472422db9
MalayKedia <malay.kedia@gmail.com>
2024-06-06 19:20:40 +0000

    csv print corrected

--- week_01/examples/csv/pic.cc
+++ week_01/examples/csv/pic.cc
@@ -22,7 +22,10 @@ void CSV::print(std::ostream &os) const {
 
 void Header::print(std::ostream &os) const {
     for (auto entry : *head_entries) {
-        os << *entry << ",";
+        os << *entry;
+        if (entry != head_entries->back()) {
+            os << ",";
+        }
     }
     os << std::endl;
 }
@@ -31,23 +34,26 @@ void DataRow::print(std::ostream &os) const {
     for (auto data : *row_data) {
         switch (data->first) {
             case DATA_INT:
-                os << *(int*)data->second << ",";
+                os << *(int*)data->second ;
                 break;
             case DATA_FLOAT:
-                os << *(float*)data->second << ",";
+                os << *(float*)data->second ;
                 break;
             case DATA_STRING:
-                os << *(string*)data->second << ",";
+                os << *(string*)data->second ;
                 break;
             case DATA_TRUE:
-                os << "true,";
+                os << "true";
                 break;
             case DATA_FALSE:
-                os << "false,";
+                os << "false";
                 break;
             case DATA_NULL:
-                os << "null,";
+                os << "null";
                 break;
+        }
+        if (data != row_data->back()) {
+            os << ",";
         }
     }
     os << std::endl;
